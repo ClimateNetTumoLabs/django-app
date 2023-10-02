72bc2dac5875857cd2ede0a07bcc55ccf42cb65d
ClimateNet <climatenet@TUMOs-iMac.local>
2023-10-02 14:43:07 +0400

    my last commit

--- climatenet/backend/urls.py
+++ climatenet/backend/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from backend.views import DeviceDetailView, AboutPageViewSet, DeviceDetailViewSet
+from backend.views import DeviceDetailView, FooterViewSet, AboutPageViewSet, DeviceDetailViewSet, ContactUsViewSet
 
 router = DefaultRouter()
 router.register(r'about', AboutPageViewSet)
 router.register(r'devices', DeviceDetailViewSet)
-router.register(r'footer', DeviceDetailViewSet)
+router.register(r'footer', FooterViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
