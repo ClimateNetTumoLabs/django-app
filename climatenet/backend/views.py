c89a1ec5d4eb7af4648ff848abd5eed13e65339d
ClimateNet <climatenet@TUMOs-iMac.local>
2023-10-31 15:44:00 +0400

    download url and mean

--- climatenet/backend/views.py
+++ climatenet/backend/views.py
@@ -11,6 +11,9 @@ from datetime import datetime, timedelta
 from .db_connection import establish_postgresql_connection
 from .logger import logger
 from collections import Counter
+from openpyxl import Workbook
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
 def fetch_data_with_time_range(cursor, table_name, start_date, end_date):
     """Fetch data from the database within a time range."""
@@ -80,6 +83,43 @@ def compute_group_means(df, mean_interval):
 
     return group_means
 
+
+def compute_mean_for_time_range(df, start_time, end_time, mean_interval):
+    """Compute means for each day within the given time range."""
+    num_records = len(df)
+    num_days = (end_time - start_time).days
+    mean_data = []
+
+    for i in range(num_days):
+        day_start = start_time + timedelta(days=i)
+        day_end = day_start + timedelta(days=1)
+        filtered_df = df[(df['time'] >= day_start) & (df['time'] < day_end)]
+
+        if not filtered_df.empty:
+            day_mean = {}
+            day_mean['time'] = day_start.strftime('%Y-%m-%d')
+
+            for column in df.columns:
+                if column == 'time':
+                    continue  # Skip the time column
+                if pd.api.types.is_numeric_dtype(df[column].dtype):
+                    mean_value = round(filtered_df[column].mean(), 2)
+                    day_mean[column] = mean_value
+                else:
+                    day_mean[column] = None
+
+            most_frequent_direction = Counter(filtered_df['direction']).most_common(1)
+            if most_frequent_direction:
+                day_mean['direction'] = most_frequent_direction[0][0]
+            else:
+                day_mean['direction'] = None
+
+            mean_data.append(day_mean)
+        else:
+            mean_data.append(None)
+
+    return mean_data
+
 class DeviceDetailView(generics.ListAPIView):
     serializer_class = DeviceSerializer
 
@@ -128,8 +168,9 @@ class DeviceDetailView(generics.ListAPIView):
                     else:
                         interval = (end_date - start_date).days
                         mean_interval = 4 if interval == 1 else 4 * interval
+                        group_means = compute_mean_for_time_range(df, start_date, 
+                                end_date, mean_interval)
 
-                        group_means = compute_group_means(df, mean_interval)
 
                         return Response(group_means, status=status.HTTP_200_OK)
 
@@ -168,6 +209,48 @@ class DeviceDetailView(generics.ListAPIView):
             logger.error(f"An error occurred: {e}")
             return Response({'error': 'An error occurred while fetching the data.'},
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+def download_data_excel(request, device_id, start_time_str, end_time_str):
+    try:
+        # Parse start_time and end_time from the provided strings
+        start_time = datetime.strptime(start_time_str, '%Y-%m-%d')  
+        end_time = datetime.strptime(end_time_str, '%Y-%m-%d') + timedelta(days=1)
+
+        cursor = establish_postgresql_connection().cursor()
+        table_name = f'device{str(device_id)}'
+
+        rows = fetch_data_with_time_range(cursor, table_name, start_time, end_time)
+
+        # Convert the rows to a list of dictionaries
+        data = preprocess_device_data(rows)
+
+        # Create a new Excel workbook and add a worksheet
+        wb = Workbook()
+        ws = wb.active
+
+        # Add a header row with column names
+        header = ['Time', 'Light', 'Temperature', 'Pressure', 'Humidity', 'PM1', 
+                'PM2.5', 'PM10', 'Speed', 'Rain', 'Direction']
+        ws.append(header)
+
+        # Add data rows to the worksheet
+        for row in data:
+            ws.append(list(row.values()))
+
+        # Create an HTTP response with the Excel content
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = f'filename=data_{start_time_str}_{end_time_str}.xlsx'
+
+        # Save the workbook content to the response
+        wb.save(response)
+
+        return response
+
+    except Exception as e:
+        return Response({'error': 'An error occurred while generating the Excel file'},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class AboutPageViewSet(viewsets.ModelViewSet):
