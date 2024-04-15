03e145f9496f65ad5f5e57a07011bb00f75d069b
sarzumanyan <sona.arzumanyan42@gmail.com>
2024-04-15 18:50:46 +0400

    urls rearranging

--- climatenet/climatenet/urls.py
+++ climatenet/climatenet/urls.py
@@ -6,12 +6,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='main-page'),
-    path('admin/', admin.site.urls),
-    path('device/<str:device_id>/', TemplateView.as_view(template_name='index.html'), name='device-detail-page'),
     path('about/', TemplateView.as_view(template_name='index.html'), name='about-page'),
-    path('device_inner/', include('backend.urls')),  # Include your app's URLs here
+    path('device/<str:device_id>/', TemplateView.as_view(template_name='index.html'), name='device-detail-page'),
+    path('admin/', admin.site.urls),
+    path('device_inner/', include('backend.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
