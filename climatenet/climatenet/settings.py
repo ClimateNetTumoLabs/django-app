2a8d5f418a690bcb65c8bc96df3f4be5b1cd6ee9
sarzumanyan <sona.arzumanyan42@gmail.com>
2024-04-22 13:06:39 +0400

    venv checking

--- climatenet/climatenet/settings.py
+++ climatenet/climatenet/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = False
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = ['climatenet.am', 'dev.climatenet.am', "https://dev.climatenet.am", '127.0.0.1', "localhost", ]
+ALLOWED_HOSTS = ['climatenet.am', 'dev.climatenet.am', "https://dev.climatenet.am", '127.0.0.1', "localhost", '3.89.111.184']
 APPEND_SLASH = True
 
 DATABASES = {
@@ -82,7 +82,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://climatenet.am",
-    "https://dev.climatenet.am"
+    "https://dev.climatenet.am",
+    "http://3.89.111.184"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
