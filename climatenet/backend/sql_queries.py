603b0a2f28502e465f47e1c9af51e09ea49ff25e
eriktumosaryan <erik.saryan@tumo.org>
2024-06-17 17:30:48 +0400

    change day nigh

--- climatenet/backend/sql_queries.py
+++ climatenet/backend/sql_queries.py
@@ -12,26 +12,24 @@ HOURLY_DATA_QUERY = '''
 '''
 
 # Fetch data for a custom time range
-CUSTOM_TIME_QUERY = '''SELECT 
-                TO_CHAR("time", 'YYYY-MM-DD') AS date,
-                CASE 
-                    WHEN EXTRACT(HOUR FROM "time") BETWEEN 0 AND 11 THEN 'night'
-                    WHEN EXTRACT(HOUR FROM "time") BETWEEN 12 AND 23 THEN 'day'
-                END AS time_interval,
-                {columns}
-            FROM {table_name}
-            WHERE "time" BETWEEN 
-                COALESCE(
-                    (SELECT MIN("time") FROM {table_name} WHERE "time" >= '{start_date}'), 
-                    '{start_date}'
-                ) 
-                AND 
-                COALESCE(
-                    (SELECT MAX("time") FROM {table_name} WHERE "time" <= '{end_date}'), 
-                    '{end_date}'
-                )
-            GROUP BY TO_CHAR("time", 'YYYY-MM-DD'), time_interval;
-            '''
+CUSTOM_TIME_QUERY = '''
+SELECT 
+    DATE_TRUNC('hour', "time") AS hour ,
+    {columns}
+FROM {table_name}
+WHERE "time" BETWEEN 
+    COALESCE(
+        (SELECT MIN("time") FROM {table_name} WHERE "time" >= '{start_date}'), 
+        '{start_date}'
+    ) 
+    AND 
+    COALESCE(
+        (SELECT MAX("time") FROM {table_name} WHERE "time" <= '{end_date}'), 
+        '{end_date}'
+    )
+GROUP BY hour
+ORDER BY hour;
+'''
 
 # Nearby data fetching query
 NEARBY_DATA_QUERY = "SELECT temperature FROM {table_name} ORDER BY id DESC LIMIT 1;"
